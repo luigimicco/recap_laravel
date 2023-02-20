1e3c31e770a2b5d4db35fbbb8aea42b92647a9a9
luigimicco <luigi.micco@gmail.com>
2023-02-20 14:07:46 +0100

    fix navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,12 @@
-<header class="container-fluid">
-  <div class="row">
-      <div class="col-12">
-          <nav class="navbar navbar-expand-lg bg-light">
-              <div class="container-fluid">
-                  <h1>My library</h1>
-                  <span class="navbar-text">
-                    <a class="btn btn-link" href="{{ route('home') }}">Home</a>
-                    <a  class="btn btn-link" href="{{ route('books.index') }}">Books</a>
-                    <a  class="btn btn-link" href="{{ route('books.trashed') }}">Recycled bin</a>
-                  </span>
-              </div>
+<header>
+  <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+          <h1>My library</h1>
+          <span class="navbar-text">
+            <a class="btn btn-link" href="{{ route('home') }}">Home</a>
+            <a  class="btn btn-link" href="{{ route('books.index') }}">Books</a>
+            <a  class="btn btn-link" href="{{ route('books.trashed') }}">Recycled bin</a>
+          </span>
       </div>
-      </nav>
-  </div>
-  </div>
+  </nav>
 </header>
